55e63b348c6c028d7ff3d25cbec0534582ee2418
Nessvah <info@silviavanessa.dev>
2023-04-04 11:04:50 +0100

    add user data

---
+++ userData.js
@@ -0,0 +1,54 @@
+const userData = {
+  1: {
+    name: "John",
+    picture: "/public/assets/img/dog1.webp",
+    status: "How you doing?",
+  },
+  2: {
+    name: "John",
+    picture: "/public/assets/img/dog2.webp",
+    status: "How you doing?",
+  },
+  3: {
+    name: "John",
+    picture: "/public/assets/img/dog3.webp",
+    status: "How you doing?",
+  },
+  4: {
+    name: "John",
+    picture: "/public/assets/img/dog4.webp",
+    status: "How you doing?",
+  },
+  5: {
+    name: "John",
+    picture: "/public/assets/img/dog5.webp",
+    status: "How you doing?",
+  },
+  6: {
+    name: "John",
+    picture: "/public/assets/img/dog6.webp",
+    status: "How you doing?",
+  },
+  7: {
+    name: "John",
+    picture: "/public/assets/img/dog7.webp",
+    status: "How you doing?",
+  },
+  8: {
+    name: "John",
+    picture: "/public/assets/img/dog8.webp",
+    status: "How you doing?",
+  },
+  9: {
+    name: "John",
+    picture: "/public/assets/img/dog9.webp",
+    status: "How you doing?",
+  },
+  10: {
+    name: "John",
+    picture: "/public/assets/img/dog10.webp",
+    status: "How you doing?",
+  },
+};
+
+export default userData;
